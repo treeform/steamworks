f3d837027271a243018290baa65cd009e0b193c5
treeform <starplant@gmail.com>
2019-02-16 08:49:37 -0800

    Updated steamworks. Better docs.

--- README.md
+++ README.md
@@ -9,19 +9,42 @@ Steamworks is a nim Wrapper for Valve’s Steamworks API under the MIT license!
 Go to https://partner.steamgames.com/downloads/list and download Steamworks SDK is version 1.42, released on Jan 3, 2018 into your repo.
 Uncompress it, structure should look some thing like this:
 
-steamworks
- * src
- * steamworks.nimbe
+steamworks/
+ * src/
+ * tests/
+ * steamworks.nimble
  * README.md
  * LICENSE
  * steamworks_sdk_142
 
 ### Step 2 - Make sure to use `cpp --cc:vcc`
 
- You need to use C++ mode and the Microsoft Visual C++ compiler:
+You need to use C++ mode and the Microsoft Visual C++ compiler:
 
- `nim cpp -r --cc:vcc`
+`nim cpp -r --cc:vcc`
 
 ### Step 3 - steam_appid.txt
 
-You need to add steam_appid.txt file to the directory you run this from. It needs to contain your Steam App ID number without spaces.
+You need to add steam_appid.txt file to the directory you run this from. It needs to contain your SteamApp ID number without spaces.
+
+## Step 4 - run the test script
+
+`nim c -r "tests\test.nim"`
+
+
+## Features
+
+This wrapper supports following things:
+
+* Getting steam account and username.
+* Getting app installed dir.
+* Gettting vac ban status.
+* Getting app build ID.
+* Getting Beta Name.
+* Getting Current Language.
+* Getting DLC count, purchise information, and install progress.
+* Getting Launch Query Param and ability to get invited to a game.
+* Activating the overlay. Overlay allows you to chat with friends, configure controllers, take screenshots, stream the game, buy DLC and more...
+* Getting friends count and their account, names and ingame information.
+* Ability to listen for friend message and replying (sending first message is not possible with steam SDK)
+* Getting number of current players playing the game.
--- tests/test.nim
+++ tests/test.nim
@@ -7,12 +7,18 @@ if not fileExists("steam_appid.txt"):
 var appId = uint32 parseInt(readFile("steam_appid.txt").strip()) # this is a test appId, use ID you get after regesterting on steam.
 initSteamworks(appId)
 
+echo "---------------------"
+echo "Testing Basics"
+echo "---------------------"
 
 echo "Did steam init ok: ", steamOk
 echo "steamAccountId: ", steamAccountId
 echo "steamName: ", steamName
 
-echo ""
+echo "---------------------"
+echo "Testing Stuff"
+echo "---------------------"
+
 echo "getAppInstallDir: ", steamApps.getAppInstallDir(appId)
 echo "isVACBanned: ", steamApps.BIsVACBanned()
 echo "getAppBuildId: ", steamApps.GetAppBuildId()
@@ -23,56 +29,54 @@ var punBytesDownloaded, punBytesTotal: uint64
 discard steamApps.GetDlcDownloadProgress(472490, addr punBytesDownloaded, addr punBytesTotal)
 echo "GetDlcDownloadProgress: ", punBytesDownloaded, "/", punBytesTotal
 echo "GetEarliestPurchaseUnixTime: ", steamApps.GetEarliestPurchaseUnixTime(appId)
-echo "GetLaunchQueryParam: ", steamApps.GetLaunchQueryParam("test")
-
+echo "GetLaunchQueryParam: ", steamApps.GetLaunchQueryParam("invite")
 echo "ActivateGameOverlay: DialogType.friends"
+echo "Pop friends"
 steamFriends.ActivateGameOverlay(DialogType.friends)
 
 
-echo ""
+echo "---------------------"
+echo "Testing Friends"
+echo "---------------------"
+var friends = getSteamFriends()
+for friend in friends:
+  echo "  name: ", friend.name
+  echo "    accountId: ", friend.accountId
+  echo "    state: ", friend.state
+  echo "    playingAppId: ", friend.playingAppId
+setListenForFriendsMessages(true)
+for friend in friends:
+  if friend.name == "treeform":
+    sendMessageToFrind(friend, "test1")
+    echo steamFriends.InviteUserToGame(friend.steamId, "this is how it is done")
 
-# echo "Steam Friends"
-# var friends = getSteamFriends()
-# for friend in friends:
-#   echo "  name: ", friend.name
-#   echo "    accountId: ", friend.accountId
-#   echo "    state: ", friend.state
-#   echo "    playingAppId: ", friend.playingAppId
-
-
-# setListenForFriendsMessages(true)
-# for friend in friends:
-#   if friend.name == "Godde":
-#     echo "found you", friend.name
-#     sendMessageToFrind(friend, "test1")
-#     echo steamFriends.InviteUserToGame(friend.steamId, "this is how it is done")
-
-echo "call backs"
+echo "---------------------"
+echo "Testing Callbacks"
+echo "---------------------"
 
 steamUserStats.getNumberOfCurrentPlayers proc(number: int) =
   echo "NumberOfCurrentPlayers: ", number
 
-# discard steamFriends.SetListenForFriendsMessages(true)
-# steamFriends.onGameConnectedFriendChatMsg proc(steamId: CSteamID, msgId: cint) =
-#   echo "chat msgId: ", msgId
-#   const bufferSize: int = 4086
-#   var buffer: cstring = cast[cstring](alloc(bufferSize))
-#   var entryType: EChatEntryType
-#   discard steamFriends.GetFriendMessage(steamId, msgId, buffer, bufferSize, addr entryType)
-#   echo "[", entryType, "]: ", buffer
-#   if entryType == ChatMsg:
-#     echo "steamFriends: ", steamFriends.ReplyToFriendMessage(steamId, "test " & $msgId)
+discard steamFriends.SetListenForFriendsMessages(true)
+steamFriends.onGameConnectedFriendChatMsg proc(steamId: CSteamID, msgId: cint) =
+  echo "chat msgId: ", msgId
+  const bufferSize: int = 4086
+  var buffer: cstring = cast[cstring](alloc(bufferSize))
+  var entryType: EChatEntryType
+  discard steamFriends.GetFriendMessage(steamId, msgId, buffer, bufferSize, addr entryType)
+  echo "[", entryType, "]: ", buffer
+  if entryType == ChatMsg:
+    echo "steamFriends: ", steamFriends.ReplyToFriendMessage(steamId, "test " & $msgId)
 
 steamFriends.onGameOverlayActivated proc(active: bool) =
   echo "overlay state: ", active
 
-echo "visit steam://run/" & $appId & "//?invite=value1;host=value2;param3=value3;"
+echo "visit steam://run/" & $appId & "//?invite=value1;host=value2;param3=value3; to change query params"
 steamApps.onNewLaunchQueryParameters proc() =
   echo "invite: ", steamApps.GetLaunchQueryParam("invite")
   echo "host: ", steamApps.GetLaunchQueryParam("host")
 
+echo "Waiting for messages, press Ctrl-C to exit."
 while true:
-  #echo "running callbacks"
   SteamAPI_RunCallbacks()
-  #GetNumberOfCurrentPlayers()
   sleep(100)
